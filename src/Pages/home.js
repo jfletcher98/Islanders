d8fac7e31320278cbfc00437de1691d80913ec1a
imjncarlson <imjncarlson@gmail.com>
2020-02-22 12:37:56 -0700

    Added router depend

---
+++ src/Pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {button} from "bloomer";
+//import styled from '@emotion/styled';
+
+export const Home = () => {
+    return (
+    <>
+    
+    <h1>This is our website</h1>
+
+    </>
+    );
+}
+
